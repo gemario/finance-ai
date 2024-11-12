7461d5703b149bbe96402c431c98712c5bb2bf86
gemario <gmario.sh@gmail.com>
2024-11-12 00:18:18 -0300

    feat: create add transaction dialog

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,8 +1,7 @@
-import { ArrowDownUpIcon } from "lucide-react";
-import { Button } from "../_components/ui/button";
 import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { TransactionColumns } from "./_columns";
+import AddTransactionButton from "../_components/add-transaction-button";
 
 export default async function TransactionsPage() {
   const transactions = await db.transaction.findMany({});
@@ -10,10 +9,7 @@ export default async function TransactionsPage() {
     <div className="space-y-6 p-6">
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
-        <Button className="rounded-full font-bold">
-          Adicionar transação
-          <ArrowDownUpIcon />
-        </Button>
+        <AddTransactionButton />
       </div>
       <DataTable columns={TransactionColumns} data={transactions} />
     </div>
